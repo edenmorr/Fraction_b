ee8658e65a4a3befc6a422abe3823409b001166d
edenmorr <93930203+edenmorr@users.noreply.github.com>
2023-05-10 12:19:39 +0300

    Add files via upload

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -7,155 +7,334 @@
 #include <stdio.h>
 using namespace std;
 namespace ariel{
+
     Fraction::Fraction(){
         this->denominator=1;
         this->numerator=0;
     }
-    Fraction::Fraction(int numerator, int denominator)
+
+    Fraction::Fraction(int numerator, int denominator):numerator(numerator) , denominator(denominator)
     {
         if (denominator == 0)
         {
-            throw __EXCEPTION__("error - canot divide by zero");
+            throw invalid_argument("error - canot divide by zero");
         }
-        this->numerator = numerator;
-        this->denominator = denominator;
-    }
-   
-    void Fraction::gcd(){}
 
-        Fraction operator+(const Fraction &frack1,const Fraction &frack2){
-        Fraction fraction1;
-         return fraction1;
+        if(numerator>0 && denominator<0)
+        {
+           this->setNumerator(numerator*-1);
+           this->setDenominator(denominator*-1);
         }
-         Fraction operator+(const Fraction &frack,const float num){
-         Fraction fraction1;
-         return fraction1;
-         }
-         Fraction operator+( float num, const Fraction &frack){
-         Fraction fraction1;
-         return fraction1;
+
+        if(numerator<0 && denominator<0){
+              this->setNumerator(numerator*-1);
+              this->setDenominator(denominator*-1);
         }
+        }
+
+    Fraction::Fraction(float number):numerator(number*1000),denominator(1000){
+    }
+
+        Fraction float_to_frac(float num){
+        return Fraction(num*1000,1000);
+       }
+   
+        int gcd(int numerator, int denominator) {
+        while (denominator != 0) {
+        int temp = denominator;
+        denominator = numerator % denominator;
+        numerator = temp;
+        }
+         return numerator;
+        }
+    //       void exeption(int a,int b,char c)
+    // {
+    //     switch(c) {
+    //     case '+':
+    //         if((a > 0 && b > 0 && (a+b < 0)) || (a < 0 && b < 0 && (a+b > 0)))
+    //         {
+    //             throw overflow_error("overflow error");
+    //         }
+    //         break;
+    //     case '*':
+    //         if(a != 0 && b != 0)
+    //         {
+    //             int temp = a*b;
+    //             if((a != temp / b) || (b != temp / a))
+    //             {
+    //                 throw overflow_error("overflow error");
+    //             }
+    //         }
+    //         break;
+    //     }
+    //     }
+
+        Fraction operator+(const Fraction &frack1, const Fraction &frack2){
+        int result_numerator;
+        int result_denominator;
+        int l;
+         if (frack1.getDenominator()==frack2.getDenominator()){
+            result_numerator= frack1.getNumerator()+frack2.getNumerator();
+            return Fraction(result_numerator,frack1.getDenominator());
+        }
+         else{
+            result_numerator = (frack1.getNumerator())*(frack2.getDenominator()) + (frack2.getNumerator())*(frack1.getDenominator());
+            result_denominator = frack1.getDenominator()*frack2.getDenominator();
+            l = gcd(result_numerator,result_denominator);
+        }
+          return Fraction(result_numerator/l,result_denominator/l);
+        }
+        
 
         Fraction operator-(const Fraction &frack1,const Fraction &frack2){
-        Fraction fraction1;
-         return fraction1;
-         }
+        int result_numerator;
+        int result_denominator;
+        int l;
+        if (frack1.getDenominator()==frack2.getDenominator()){
+            result_numerator= frack1.getNumerator()-frack2.getNumerator();
+            return Fraction(result_numerator,frack1.getDenominator());
+        }
+        else{
+            result_numerator = (frack1.getNumerator())*(frack2.getDenominator()) - (frack2.getNumerator())*(frack1.getDenominator());
+            result_denominator = frack1.getDenominator()*frack2.getDenominator();
+            l = gcd(result_numerator,result_denominator);
+        }
+        
+         return Fraction(result_numerator/l,result_denominator/l);
+        }
+         
         Fraction operator*(const Fraction &frack1,const Fraction &frack2){
-         Fraction fraction1;
-         return fraction1;
+        int result_numerator;
+        int result_denominator;
+        int l;
+        result_denominator = frack1.getDenominator()*frack2.getDenominator();
+        result_numerator = frack1.getNumerator()*frack2.getNumerator();
+        l = gcd(result_numerator,result_denominator);
+         return Fraction(result_numerator/l,result_denominator/l);
         }
         Fraction operator/(const Fraction &frack1,const Fraction &frack2){
-         Fraction fraction1;
-         return fraction1;
+        int result_numerator;
+        int result_denominator;
+        int l;
+        result_numerator = frack1.getNumerator()*frack2.getDenominator();
+        result_denominator = frack1.getDenominator()*frack2.getNumerator();
+        if((result_denominator)==0){
+            throw runtime_error("error - canot divide by zero");
         }
-       
+        l = gcd(result_numerator,result_denominator);
+        return Fraction(result_numerator/l,result_denominator/l);
+        }
+
+        Fraction operator+(const Fraction &frack,const float num){
+        Fraction f = float_to_frac(num);
+        return operator+(frack,f);
+        }
+
         Fraction operator-(const Fraction &frack,const float num){
-         Fraction fraction1;
-         return fraction1;
+        Fraction f = float_to_frac(num);
+        return operator-(frack,f);
         }
         Fraction operator*(const Fraction &frack,const float num){
-         Fraction fraction1;
-         return fraction1;
+        Fraction f = float_to_frac(num);
+         return operator*(frack,f);
         }
         Fraction operator/(const Fraction &frack,const float num){
-         Fraction fraction1;
-         return fraction1;
+        Fraction f = float_to_frac(num);
+         return operator/(frack,f);
         }
 
-       
+        Fraction operator+( float num, const Fraction &frack){
+        Fraction f = float_to_frac(num);
+        return operator+(f,frack);
+        }
         Fraction operator-( float num, const Fraction &frack){
-         Fraction fraction1;
-         return fraction1;
+        Fraction f = float_to_frac(num);
+        return operator-(f,frack); 
         }
         Fraction operator*( float num, const Fraction &frack){
-         Fraction fraction1;
-         return fraction1;
+        Fraction f = float_to_frac(num);
+        return operator*(f,frack);
         }
         Fraction operator/( float num, const Fraction &frack){
-         Fraction fraction1;
-         return fraction1;
+        Fraction f = float_to_frac(num);
+        return operator/(f,frack);
         }
 
-        Fraction& Fraction::operator++(){
-            return *this;
+        Fraction const & Fraction::operator++(){//++i
+        Fraction f(1,1);
+        *this = operator+(*this,f);
+         return *this;
         }
-        Fraction Fraction::operator++(int){
-            return Fraction(1,2);
+
+        Fraction Fraction::operator++(int){//i++ // beacuse of lvalue
+        Fraction f(1,1);
+        Fraction frack = *this;
+        *this = operator+(*this,f);
+            return frack;
         } 
-        Fraction& Fraction::operator--(){
-            return *this;
+        
+        Fraction const & Fraction::operator--(){//--i // beacuse of lvalue
+        Fraction f(1,1);
+        *this = operator-(*this,f);
+         return *this;
         } 
-        Fraction Fraction::operator--(int){
-            return Fraction(1,2);
+        Fraction Fraction::operator--(int){//i--
+        Fraction f(1,1);
+        Fraction frack = *this;
+        *this = operator-(*this,f);
+            return frack;
         } 
 
         bool operator==(const Fraction &frack1, const Fraction &frack2){
+       
+        int l;
+        int ln;
+        l = gcd(frack1.getNumerator(),frack1.getDenominator());
+        ln = gcd(frack2.getNumerator(),frack2.getDenominator());
+            if(frack1.getNumerator()/l==frack2.getNumerator()/ln && frack1.getDenominator()/l==frack2.getDenominator()/ln){
+            return true;
+            }
             return false;
         }
-        bool operator!=(const Fraction &frack1, const Fraction &frack2){
-            return false;
-        }
+
         bool operator>(const Fraction &frack1, const Fraction &frack2){
+            int frack1_mone;
+            int frack2_mone;
+           // if (operator==(frack1,frack2)==false){
+                if(frack1.getDenominator()==frack2.getDenominator() && frack1.getNumerator()> frack2.getNumerator())
+                    return true;
+                else{
+                frack1_mone = frack1.getNumerator()*frack2.getDenominator();
+                frack2_mone = frack2.getNumerator()*frack1.getDenominator();
+                if(frack1_mone>frack2_mone){
+                    return true;
+                }
+                }
+           // }
             return false;
         }
         bool operator<(const Fraction &frack1, const Fraction &frack2){
-            return false;
-        }
-        bool operator>=(const Fraction &frack1, const Fraction &frack2){
-            return false;
-        }
-        bool operator<=(const Fraction &frack1, const Fraction &frack2){
-            return false;
+            int frack1_mone;
+            int frack2_mone;
+            //if (operator==(frack1,frack2)==false){
+                if(frack1.getDenominator()==frack2.getDenominator() && frack1.getNumerator()< frack2.getNumerator()){
+                    return true;
+                }
+                else{
+                frack1_mone = frack1.getNumerator()*frack2.getDenominator();
+                frack2_mone = frack2.getNumerator()*frack1.getDenominator();
+                if(frack1_mone<frack2_mone){
+                    return true;
+                }
+
+                }
+           // }
+            return false;;
         }
 
+        bool operator>=(const Fraction &frack1, const Fraction &frack2){
+            int frack1_mone;
+            int frack2_mone;
+                if(frack1.getDenominator()==frack2.getDenominator() && frack1.getNumerator()>= frack2.getNumerator())
+                    return true;
+                else{
+                frack1_mone = frack1.getNumerator()*frack2.getDenominator();
+                frack2_mone = frack2.getNumerator()*frack1.getDenominator();
+                if(frack1_mone>=frack2_mone){
+                    return true;
+                }
+                }
+            return false;
+                }
+        bool operator<=(const Fraction &frack1, const Fraction &frack2){
+               int frack1_mone;
+            int frack2_mone;
+       
+                if(frack1.getDenominator()==frack2.getDenominator() && frack1.getNumerator()<= frack2.getNumerator())
+                    return true;
+                else{
+                frack1_mone = frack1.getNumerator()*frack2.getDenominator();
+                frack2_mone = frack2.getNumerator()*frack1.getDenominator();
+                if(frack1_mone<=frack2_mone){
+                    return true;
+                }
+                }
+          
+            return false;;
+        }
+      
         bool operator==(const Fraction &frack, float num){
-            return false;
+        
+        Fraction f = float_to_frac(num);
+        int l = gcd(f.getNumerator(),f.getDenominator());
+        int g = gcd(f.getNumerator(),f.getDenominator());
+
+        return operator==(frack/l,f/g);
         }
-        bool operator!=(const Fraction &frack, float num){
-            return false;
-        }
+
         bool operator>(const Fraction &frack, float num){
-            return false;
-        }
+        Fraction f = float_to_frac(num);
+        return operator>(frack,f);
+        }        
         bool operator<(const Fraction &frack, float num){
-            return false;
-        }
+         Fraction f = float_to_frac(num);
+        return operator<(frack,f);
+        }  
         bool operator>=(const Fraction &frack, float num){
-            return false;
-        }
+        Fraction f = float_to_frac(num);
+        return operator>=(frack,f);
+        }  
         bool operator<=(const Fraction &frack, float num){
-            return false;
-        }
+        Fraction f = float_to_frac(num);
+        return operator<=(frack,f);
+        }  
 
         bool operator==(float num, const Fraction &frack){
-            return false;
-        }
-        bool operator!=(float num, const Fraction &frack){
-            return false;
-        }
-        bool operator>(float num, const Fraction &frack){
-            return false;
-        }
-        bool operator<(float num, const Fraction &frack){
-            return false;
-        }
-        bool operator>=(float num, const Fraction &frack){
-            return false;
-        }
-        bool operator<=(float num, const Fraction &frack){
-            return false;
+        Fraction f = float_to_frac(num);
+
+        int l = gcd(f.getNumerator(),frack.getDenominator());
+        int g = gcd(f.getNumerator(),frack.getDenominator());
+
+        return operator==(frack/l,f/g);
         }
 
+        bool operator>(float num, const Fraction &frack){
+        Fraction f = float_to_frac(num);
+        return operator>(f,frack);
+        }        
+        bool operator<(float num, const Fraction &frack){
+        Fraction f = float_to_frac(num);
+        return operator<(f,frack);
+        } 
+        bool operator>=(float num, const Fraction &frack){
+       Fraction f = float_to_frac(num);
+        return operator>=(f,frack);
+        } 
+        bool operator<=(float num, const Fraction &frack){
+         Fraction f = float_to_frac(num);
+        return operator<=(f,frack);
+        } 
+
      // The << operator to print a fraction
-    std::ostream &operator<<(std::ostream &stream, const Fraction &fruc){
-        //need to perform
-        return stream;
+    std::ostream &operator<<(std::ostream &stream, const Fraction &frac){
+        return stream << frac.getNumerator() << '/' << frac.getDenominator();
     }
 
     // The >> operator to read a fraction 
     std::istream& operator>>(std::istream& stream, Fraction& frac){
-        //need to perform
-        return stream;
+    int numer,denomer;
+    stream >> numer >> denomer;
+
+    if(stream.fail()){
+        throw runtime_error("error with reading string");
+    }
+    if(denomer==0){
+        throw runtime_error("invalid input");
+    }
+     Fraction temp(numer,denomer);
+        frac.numerator = temp.numerator;
+        frac.denominator = temp.denominator;
+    return stream;
+
     }
 }
-
--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -13,11 +13,31 @@ namespace ariel{
     private:
     int numerator;
     int denominator;
-    void gcd();//ensures that the Fraction object is in its simplest form, where the numerator and denominator have no common factors other than 1.
-
+    
     public:
-        Fraction(int numerat, int denomin);
-        Fraction();
+    Fraction(int numerat, int denomin);
+    Fraction(float number);
+    Fraction();
+
+    void setNumerator(int numerator1) {
+        numerator = numerator1;
+    }
+
+    int getNumerator() const {
+        return numerator;
+    }
+
+    void setDenominator(int denom) {
+        denominator = denom;
+    }
+
+    int getDenominator() const {
+        return denominator;
+    }
+    
+      friend int gcd(int numerator, int denominator);//ensures that the Fraction object is in its simplest form, where the numerator and denominator have no common factors other than 1.
+        friend Fraction float_to_frac(float num);
+
         // im compering and doing operetion numbers with objects because the exemple in the demo 
         friend Fraction operator+(const Fraction& frack1, const Fraction& frack2);
         friend Fraction operator-(const Fraction& frack1, const Fraction& frack2);
@@ -34,34 +54,28 @@ namespace ariel{
         friend Fraction operator*(float num, const Fraction&frack);
         friend Fraction operator/(float num, const Fraction&frack);
     
-        Fraction& operator++(); // pre-increment
+        Fraction const & operator++(); // pre-increment
         Fraction operator++(int); // post-increment
-        Fraction& operator--(); // pre-decrement
+        Fraction const & operator--(); // pre-decrement
         Fraction operator--(int); // post-decrement
 
         friend bool operator==(const Fraction &frack1, const Fraction &frack2);
-        friend bool operator!=(const Fraction &frack1, const Fraction &frack2);
         friend bool operator>(const Fraction &frack1, const Fraction &frack2);
         friend bool operator<(const Fraction &frack1, const Fraction &frack2);
         friend bool operator>=(const Fraction &frack1, const Fraction &frack2);
         friend bool operator<=(const Fraction &frack1, const Fraction &frack2);
 
         friend bool operator==(const Fraction &frack, float num);
-        friend bool operator!=(const Fraction &frack, float num);
         friend bool operator>(const Fraction &frack, float num);
         friend bool operator<(const Fraction &frack, float num);
         friend bool operator>=(const Fraction &frack, float num);
         friend bool operator<=(const Fraction &frack, float num);
 
         friend bool operator==( float num, const Fraction &frack);
-        friend bool operator!=( float num, const Fraction &frack);
         friend bool operator>( float num, const Fraction &frack);
         friend bool operator<( float num, const Fraction &frack);
         friend bool operator>=( float num, const Fraction &frack);
         friend bool operator<=( float num, const Fraction &frack);
-
-        // friend ostream& operator<<(ostream &out, Fraction &frack);//>> is used to read a Fraction
-        // friend istream& operator>>(istream &in, Fraction &frack);// is used to output the Fraction////
 
         operator string() const{
             return to_string(this->numerator)+"/"+to_string(this->denominator);
